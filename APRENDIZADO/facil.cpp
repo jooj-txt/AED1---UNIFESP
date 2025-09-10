6c7370a6d38080d4ac57c2c5f95c543601f2033f
jooj-txt <rubesnjose@gmail.com>
2025-09-09 22:34:11 -0300

    a

---
+++ APRENDIZADO/facil.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int A, B;
+    int R;
+    int X;
+    
+    cin >> A;
+    cin >> B;
+    
+    R = A + B;
+    
+    cout << "X =" << R << endl;
+    
+    
+ 
+
+    return 0;
+}
